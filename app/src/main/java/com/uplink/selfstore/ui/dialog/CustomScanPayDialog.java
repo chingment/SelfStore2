e1ebc209a15e51143544547c090f534cb7955735
qqw <qqw@uplink.net.cn>
2019-12-08 16:46:16 +0800

    s

--- app/src/main/java/com/uplink/selfstore/ui/dialog/CustomScanPayDialog.java
+++ app/src/main/java/com/uplink/selfstore/ui/dialog/CustomScanPayDialog.java
@@ -62,19 +62,19 @@ public class CustomScanPayDialog extends Dialog {
 
 
         this.txt_payamount.setText(chargeAmount);
+        this.img_payqrcode.setImageBitmap(BitmapUtil.createQrCodeBitmap(payUrl));
+
         switch (payCaller) {
             case 10:
-                this.img_payqrcode.setImageBitmap(BitmapUtil.createQrCodeBitmapAndLogo(payUrl, BitmapFactory.decodeResource(mContext.getResources(), R.drawable.icon_payway_wechat3)));
+                //this.img_payqrcode.setImageBitmap(BitmapUtil.createQrCodeBitmapAndLogo(payUrl, BitmapFactory.decodeResource(mContext.getResources(), R.drawable.icon_payway_wechat3)));
                 this.icon_payway_z_wechat.setVisibility(View.VISIBLE);
                 this.icon_payway_z_zhifubao.setVisibility(View.GONE);
                 break;
             case 20:
-                this.img_payqrcode.setImageBitmap(BitmapUtil.createQrCodeBitmapAndLogo(payUrl, BitmapFactory.decodeResource(mContext.getResources(), R.drawable.icon_payway_zhifubao3)));
                 this.icon_payway_z_wechat.setVisibility(View.GONE);
                 this.icon_payway_z_zhifubao.setVisibility(View.VISIBLE);
                 break;
             case 30:
-                this.img_payqrcode.setImageBitmap(BitmapUtil.createQrCodeBitmap(payUrl));
                 this.icon_payway_z_wechat.setVisibility(View.VISIBLE);
                 this.icon_payway_z_zhifubao.setVisibility(View.VISIBLE);
                 break;
@@ -94,10 +94,10 @@ public class CustomScanPayDialog extends Dialog {
         txt_payamount = (TextView) this.layoutRes.findViewById(R.id.txt_payamount);
         img_payqrcode = (ImageView) this.layoutRes.findViewById(R.id.img_payqrcode);
         txt_paytips = (TextView) this.layoutRes.findViewById(R.id.txt_paytips);
+        icon_payway_z_wechat =  this.layoutRes.findViewById(R.id.icon_payway_z_wechat);
+        icon_payway_z_zhifubao =this.layoutRes.findViewById(R.id.icon_payway_z_zhifubao);
         txt_payseconds = (TextView) this.layoutRes.findViewById(R.id.txt_payseconds);
-        btn_close = (View) this.layoutRes.findViewById(R.id.btn_close);
-        icon_payway_z_wechat = findViewById(R.id.icon_payway_z_wechat);
-        icon_payway_z_zhifubao = findViewById(R.id.icon_payway_z_zhifubao);
+        btn_close =this.layoutRes.findViewById(R.id.btn_close);
     }
 
     @Override
