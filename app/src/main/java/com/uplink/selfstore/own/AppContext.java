0b7ced9d6ba829bfcfb478436c23bd374b56d7cf
qqw <qqw@uplink.net.cn>
2019-12-15 21:05:29 +0800

    sd

--- app/src/main/java/com/uplink/selfstore/own/AppContext.java
+++ app/src/main/java/com/uplink/selfstore/own/AppContext.java
@@ -105,7 +105,7 @@ public class AppContext extends Application {
         }
 
 
-        return DEVICE_ID+"1";
+        return DEVICE_ID;
 
 //        String DEVICE_ID = "000000000000000";
 //        try {
