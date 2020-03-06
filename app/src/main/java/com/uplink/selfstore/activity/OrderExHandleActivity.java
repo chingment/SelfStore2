848102635df2e3dee6e7eb9662f99d8e6fa04d05
qqw <qqw@uplink.net.cn>
2020-03-06 14:14:34 +0800

    dasd

--- app/src/main/java/com/uplink/selfstore/activity/OrderExHandleActivity.java
+++ app/src/main/java/com/uplink/selfstore/activity/OrderExHandleActivity.java
@@ -1,17 +1,16 @@
 package com.uplink.selfstore.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.uplink.selfstore.R;
 import com.uplink.selfstore.ui.swipebacklayout.SwipeBackActivity;
 
-public class ExOrderHandleActivity extends SwipeBackActivity implements View.OnClickListener {
-    private static final String TAG = "ExOrderHandleActivity";
+public class OrderExHandleActivity extends SwipeBackActivity implements View.OnClickListener {
+    private static final String TAG = "OrderExHandleActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exorderhandle);
+        setContentView(R.layout.activity_orderexhandle);
     }
 }
