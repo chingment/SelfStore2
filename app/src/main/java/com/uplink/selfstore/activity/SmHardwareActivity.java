e33b4426791b6546a256ba0240cb212d853e55c7
qqw <qqw@uplink.net.cn>
2020-04-22 18:02:50 +0800

    dsd

--- app/src/main/java/com/uplink/selfstore/activity/SmHardwareActivity.java
+++ app/src/main/java/com/uplink/selfstore/activity/SmHardwareActivity.java
@@ -428,7 +428,7 @@ public class SmHardwareActivity extends SwipeBackActivity implements View.OnClic
                                 showToast("摄像头数量为0");
                                 return;
                             }
-                            chuhuokou_camera = Camera.open(0);
+                            chuhuokou_camera = Camera.open(2);
                             if (chuhuokou_camera == null) {
                                 showToast("出货口摄像头对象为空");
                                 return;
