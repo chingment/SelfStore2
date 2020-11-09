553215f61ee63617670f958a67e717877d25a292
qqw <qqw@uplink.net.cn>
2020-11-09 11:33:23 +0800

    d

--- app/src/main/java/com/uplink/selfstore/activity/SmRescueToolActivity.java
+++ app/src/main/java/com/uplink/selfstore/activity/SmRescueToolActivity.java
@@ -1,19 +1,44 @@
 package com.uplink.selfstore.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.uplink.selfstore.BuildConfig;
 import com.uplink.selfstore.R;
+import com.uplink.selfstore.activity.adapter.NineGridItemAdapter;
+import com.uplink.selfstore.http.HttpResponseHandler;
+import com.uplink.selfstore.model.api.ApiResultBean;
+import com.uplink.selfstore.model.api.CabinetBean;
+import com.uplink.selfstore.model.api.Result;
+import com.uplink.selfstore.model.common.NineGridItemBean;
+import com.uplink.selfstore.model.common.NineGridItemType;
 import com.uplink.selfstore.ostCtrl.OstCtrlBySx;
 import com.uplink.selfstore.ostCtrl.OstCtrlByYs;
 import com.uplink.selfstore.ostCtrl.OstCtrlInterface;
 import com.uplink.selfstore.own.AppManager;
+import com.uplink.selfstore.own.Config;
+import com.uplink.selfstore.ui.dialog.CustomConfirmDialog;
+import com.uplink.selfstore.ui.my.MyGridView;
 import com.uplink.selfstore.ui.swipebacklayout.SwipeBackActivity;
+import com.uplink.selfstore.utils.LogUtil;
 import com.uplink.selfstore.utils.NoDoubleClickUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SmRescueToolActivity extends SwipeBackActivity implements View.OnClickListener {
 
+    private CustomConfirmDialog dialog_confirm;
     private Button btn_ShowNav;
     private Button btn_AppExit;
     @Override
@@ -21,40 +46,124 @@ public class SmRescueToolActivity extends SwipeBackActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smrescuetool);
         setNavTtile("设置检查");
-        setNavGoBackBtnVisible(true);
+        setNavGoBackBtnVisible(false);
 
         initView();
         initEvent();
+
+
+        dialog_confirm = new CustomConfirmDialog(SmRescueToolActivity.this, "", true);
+        dialog_confirm.getBtnSure().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tag = v.getTag().toString();
+                LogUtil.e("tag:" + tag);
+                switch (tag) {
+                    case "fun.closeapp":
+                        setHideStatusBar(false);
+                        AppManager.getAppManager().AppExit(SmRescueToolActivity.this);
+                        break;
+                    case "fun.rootsys":
+                        setHideStatusBar(false);
+                        OstCtrlInterface.getInstance().reboot(SmRescueToolActivity.this);
+                        break;
+                    case "fun.exitmanager":
+                        Intent intent = new Intent(getAppContext(), InitDataActivity.class);
+                        startActivity(intent);
+                        finishAffinity();
+                        break;
+                }
+                dialog_confirm.hide();
+            }
+        });
+
+        dialog_confirm.getBtnCancle().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog_confirm.hide();
+            }
+        });
     }
 
     private void initView() {
-        btn_ShowNav=(Button) findViewById(R.id.btn_ShowNav);
-        btn_AppExit=(Button)findViewById(R.id.btn_AppExit);
+        MyGridView gridview = (MyGridView) findViewById(R.id.gridview_ninegrid);
+
+        final List<NineGridItemBean> gridviewitems = new ArrayList<NineGridItemBean>();
+        gridviewitems.add(new NineGridItemBean(getAppContext().getString(R.string.aty_smhome_ngtitle_shownav), NineGridItemType.Function, "fun.shownav", R.drawable.ic_sm_machine));
+        gridviewitems.add(new NineGridItemBean(getAppContext().getString(R.string.aty_smhome_ngtitle_checkupdateapp), NineGridItemType.Function, "fun.checkupdateapp", R.drawable.ic_sm_updateapp));
+        gridviewitems.add(new NineGridItemBean(getAppContext().getString(R.string.aty_smhome_ngtitle_closeapp), NineGridItemType.Function, "fun.closeapp", R.drawable.ic_sm_closeapp));
+        gridviewitems.add(new NineGridItemBean(getAppContext().getString(R.string.aty_smhome_ngtitle_rootsys), NineGridItemType.Function, "fun.rootsys", R.drawable.ic_sm_root));
+        gridviewitems.add(new NineGridItemBean(getAppContext().getString(R.string.aty_smhome_ngtitle_exitmanager), NineGridItemType.Function, "fun.exitmanager", R.drawable.ic_sm_exit));
+
+
+        NineGridItemAdapter nineGridItemdapter = new NineGridItemAdapter(getAppContext(), gridviewitems);
+
+        gridview.setAdapter(nineGridItemdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (!NoDoubleClickUtil.isDoubleClick()) {
+                    NineGridItemBean gridviewitem = gridviewitems.get(position);
+                    int type = gridviewitem.getType();
+                    String action = gridviewitem.getAction();
+                    Intent intent;
+                    switch (type) {
+                        case NineGridItemType.Function:
+                            intent = new Intent();
+                            switch (action) {
+                                case "fun.shownav":
+                                    OstCtrlInterface.getInstance().setHideStatusBar(SmRescueToolActivity.this,false);
+                                    break;
+                                case "fun.checkupdateapp":
+                                    intent = new Intent();
+                                    intent.putExtra("from", 2);
+                                    intent.setAction("android.intent.action.updateAppService");
+                                    sendBroadcast(intent);
+                                    break;
+                                case "fun.closeapp":
+                                    dialog_confirm.getTipsImage().setVisibility(View.GONE);
+                                    dialog_confirm.getBtnSure().setTag("fun.closeapp");
+                                    dialog_confirm.getTipsText().setText(getAppContext().getString(R.string.aty_smhome_confrimtips_closeapp));
+                                    dialog_confirm.show();
+                                    break;
+                                case "fun.rootsys":
+                                    dialog_confirm.getTipsImage().setVisibility(View.GONE);
+                                    dialog_confirm.getBtnSure().setTag("fun.rootsys");
+                                    dialog_confirm.getTipsText().setText(getAppContext().getString(R.string.aty_smhome_confrimtips_rootsys));
+                                    dialog_confirm.show();
+                                    break;
+                                case "fun.exitmanager":
+                                    dialog_confirm.getTipsImage().setVisibility(View.GONE);
+                                    dialog_confirm.getBtnSure().setTag("fun.exitmanager");
+                                    dialog_confirm.getTipsText().setText(getAppContext().getString(R.string.aty_smhome_confrimtips_exitmanager));
+                                    dialog_confirm.show();
+                                    break;
+                            }
+                    }
+                }
+            }
+        });
+
     }
 
     private void initEvent() {
-        btn_ShowNav.setOnClickListener(this);
-        btn_AppExit.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         super.onClick(v);
 
-        if (!NoDoubleClickUtil.isDoubleClick()) {
-            switch (v.getId()) {
-                case R.id.nav_back:
-                    finish();
-                    break;
-                case R.id.btn_ShowNav:
-                    OstCtrlInterface.getInstance().setHideStatusBar(SmRescueToolActivity.this,false);
-                    break;
-                case R.id.btn_AppExit:
-                    OstCtrlInterface.getInstance().setHideStatusBar(SmRescueToolActivity.this,false);
-                    AppManager.getAppManager().AppExit(SmRescueToolActivity.this);
-                    break;
-            }
-        }
+
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (dialog_confirm != null) {
+            dialog_confirm.cancel();
+        }
+    }
 }
